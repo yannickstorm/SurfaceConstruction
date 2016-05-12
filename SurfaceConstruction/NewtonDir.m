fff9589424a52b8effbdbab096a26ddc99bf6032
Yannick Poffet <yannick@poffet.net>
2016-05-12 17:25:16 +1000

    Modification of the direction newton method

--- SurfaceConstruction/NewtonDir.m
+++ SurfaceConstruction/NewtonDir.m
@@ -7,20 +7,33 @@ dir = dir/norm(dir);
 x = xIn;
 err = f_tot(1);
 i = 0;
+if err < 1e-14
+    return;
+end
 while err > 1e-14
-    i = i + 1
+    i = i + 1;
     f_tot = f_plus(x);
     fCurrent = f_tot(1);
     gradCurrent = f_tot(2:end);
     gradDir = gradCurrent' * dir;
-	x = x - fCurrent/gradDir * dir;
+% 	x = x - fCurrent/gradDir * dir;
+    x = x - fCurrent/norm(gradCurrent) * gradCurrent;
     f_tot = f_plus(x);
+    fCurrent = f_tot(1);
     gradCurrent = f_tot(2:end);
     err = abs(f_tot(1))/norm(gradCurrent);
+    sig = sign(fCurrent);
     
 %     if(i > 10)
 %        disp('asdfasdf');
 %        break;
 %     end
 end
-    
+
+gradDir = gradCurrent' * dir;
+% x = x - 2 * fCurrent/gradDir * dir;
+x = x - 2 * fCurrent/norm(gradCurrent) * gradCurrent;
+f_tot = f_plus(x);
+fCurrent = f_tot(1);
+
+sig == sign(fCurrent)    
